2d94ea3d18a307dfa99d5a6a7263f94e1f554023
Andrey Shaikin <kiwibon@yandex.ru>
2022-07-01 23:56:36 +0300

    подключаемся к БД при запуске проекта

--- app.py
+++ app.py
@@ -14,3 +14,19 @@ app = FastAPI(
 )
 
 app.include_router(router=apps_router)
+
+app.state.database = settings.DATABASE
+
+
+@app.on_event("startup")
+async def startup() -> None:
+    database_ = app.state.database
+    if not database_.is_connected:
+        await database_.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown() -> None:
+    database_ = app.state.database
+    if database_.is_connected:
+        await database_.disconnect()
